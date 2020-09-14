1470a81931046190ac0d35d75dfed18baee12bff
manieu-11 <ieumansteel@gmail.com>
2020-09-14 22:27:49 +0100

    fixing bugs and adding new feautures

--- client.py
+++ client.py
@@ -3,11 +3,20 @@ import threading
 import sys
 
 
+
+# TODO: get rid of failed multithreading and put into one loop
+# TODO: set up logic so that if there's a message from the server it automatically stops the input and prints the msg
+#       maybe using pyunpt for key pressing or some form of prioritising messages from the server
+
+
 class Client:
 
     def __init__(self):
         self.host = "127.0.0.1"
         self.port = 9009
+
+        self.server_msg = ""
+        # init var to store server messages
 
         try:
             self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -16,13 +25,13 @@ class Client:
             print("ConnectionError: could not connect to server")
             quit(ConnectionRefusedError)
             # trying to connect to server
-        except ConnectionError:
-            print("ConnectionError: could not connect to server")
-            quit(ConnectionError)
-            # trying to connect to server
         except ConnectionAbortedError:
             print("ConnectionError: could not connect to server")
             quit(ConnectionAbortedError)
+            # trying to connect to server
+        except ConnectionError:
+            print("ConnectionError: could not connect to server")
+            quit(ConnectionError)
             # trying to connect to server
 
         self.name = input("Username: ")
@@ -33,34 +42,52 @@ class Client:
         server_handler.start()
         # gets messages from server
 
-        client_handler = threading.Thread(target=self.sendMessage)
+        client_handler = threading.Thread(target=self.sendMessagesToServer)
         client_handler.start()
         # sends messages to server
 
     def getServerMessages(self):
         while True:
             try:
-                print(f"\n{self.client_socket.recv(1024).decode()}")
+                self.server_msg = self.client_socket.recv(1024).decode()
             except socket.error:
-                print("MessageError: could not receive message from server")
+                print("SocketError: could not receive message from server, socket failed")
             # trying to receive message from server
             except ConnectionAbortedError:
                 print("ConnectionError: Server disconnected")
 
-    def sendMessage(self):
+    def sendMessagesToServer(self):
         while True:
-            try:
-                message = input(f"{self.name}: ")
-                if message != "":
-                    if "QUIT" in message:
-                        self.client_socket.sendall(f"{self.name} has disconnected from the server".encode())
-                        self.client_socket.close()
-                        sys.exit()
-                    else:
-                        self.client_socket.sendall(f"{self.name}: {message}".encode())
-            except socket.error:
-                print("MessageError: Could not send message to server")
-                # trying to send message to server
+            if self.server_msg:
+                print(self.server_msg[2:-2])
+                self.server_msg = ""
+                # if the message from the server isn't empty print the message
+                # should get rid of required to press enter to get message bug
+            message = input(f"{self.name}: ")
+            # repeatedly prints out the name then input box to get messages
+
+            if message != "":
+                if "QUIT" in message:
+                    self.shutdown()
+                if "HELP" in message:
+                    print("- Simply type your text in the input then press enter \n- you may have to press enter to "
+                          "get the responses from the other clients")
+                else:
+                    self._broadcast(f"{self.name}: {message}")
+
+    def _broadcast(self, message):
+        try:
+            self.client_socket.sendall(message.encode())
+        except socket.error:
+            print("SocketError: could not send message to server, socket failed")
+        except:
+            print("MessageError: (catch all err msg) could not send message to server")
+
+    def shutdown(self):
+        self.client_socket.sendall(f"{self.name} has disconnected from the server".encode())
+        self.client_socket.close()
+        sys.exit(0)
+        # sends a message closes connection then script
 
 
 client = Client()
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import socket
-import threading
+import sys
 
 
 class Server:
@@ -15,41 +15,65 @@ class Server:
         self.listening_socket.listen(5)
         # set up the socket to get new connections
 
-        client_handler = threading.Thread(target=self.getNewConnections)
-        client_handler.start()
+        self.handle_clients()
+        # starts up the server
 
-        message_handler = threading.Thread(target=self.getClientMessage)
-        message_handler.start()
+    def handle_clients(self):
+        print("control + c to quit")
+        while True:
+            self.getNewClients()
+            self.getNewClientMessages()
+            # puts it neatly in one loop in stead of failing parallelism
+
+    def getNewClientMessages(self):
+        # gets clients messages
+        for client in self.connections:
+            message = client.recv(1024)
+            self.writeToLog(message.decode())
+
+            if message:
+                if message != "":
+                    self._broadcast(message, client)
+            elif not client:
+                break
+            else:
+                break
+
+    def getNewClients(self):
+        # gets new clients
+        connection, address = self.listening_socket.accept()
+        self.connections.append(connection)
+        # gets accepts connections and stores them in a list
+
+        username = connection.recv(1024)
+
+        self.writeToLog(f"{address} connected to the server!")
+        self.writeToLog(f"{username.decode()} has joined the chat.\n")
+
+        if connection not in self.connections:
+            connection.sendall("Welcome to the chat- Type 'QUIT' to leave - Type HELP for help".encode())
+            # so it doesn't repeatedly send the message
 
     def _broadcast(self, message, sender):
         for client in self.connections:
             if client != sender:
                 client.sendall(f"{message}\n".encode())
+                self.writeToLog(f"{message}\n")
 
-    def getNewConnections(self):
-        while True:
-            connection, address = self.listening_socket.accept()
-            self.connections.append(connection)
-            print(f"{address} connected to the server!")
-            # gets accepts connections and stores them in a list
+    def shutdown(self):
+        self._broadcast("Server: The server is being shut down", self.listening_socket)
+        # sends msg
+        for conn in self.connections:
+            conn.close()
+            # closes every connection
+        self.writeToLog("\t\tEnd of Session\t\t \n\n")
+        sys.exit(0)
 
-            username = connection.recv(1024)
-            print(f"{username.decode()} has joined the chat.\n")
-
-            self._broadcast("Welcome to the chat- Type 'QUIT' to leave - You may have to press enter after sending messages to get response".encode(), self.listening_socket)
-
-    def getClientMessage(self):
-        while True:
-            for client in self.connections:
-                message = client.recv(1024)
-                print(message.decode())
-                if message:
-                    if message != "":
-                        self._broadcast(message, client)
-                elif not client:
-                    break
-                else:
-                    break
+    @classmethod
+    def writeToLog(cls, log_message):
+        with open("log.txt", "a") as log:
+            # opens a log file to note down server activity
+            log.write(log_message + "\n")
 
 
 server = Server()
